0abc93c517287d8efb0434ed4af549564e0abfb6
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-31 15:01:13 -0300

    Remoção do carregamento do CSS pelo JS.

--- javascripts/js.js
+++ javascripts/js.js
@@ -31,20 +31,20 @@ function githubRibbon() {
 }
 
 // Optimize CSS Delivery
-var loadDeferredStyles = function() {
-    var addStylesNode = document.getElementById("deferred-styles");
-    var replacement = document.createElement("div");
-    replacement.innerHTML = addStylesNode.textContent;
-    document.body.appendChild(replacement);
-    addStylesNode.parentElement.removeChild(addStylesNode);
-};
-var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-    webkitRequestAnimationFrame || msRequestAnimationFrame;
-if (raf) {
-    raf(function() { window.setTimeout(loadDeferredStyles, 0); });
-} else {
-    window.addEventListener('load', loadDeferredStyles);
-}
+// var loadDeferredStyles = function() {
+//     var addStylesNode = document.getElementById("deferred-styles");
+//     var replacement = document.createElement("div");
+//     replacement.innerHTML = addStylesNode.textContent;
+//     document.body.appendChild(replacement);
+//     addStylesNode.parentElement.removeChild(addStylesNode);
+// };
+// var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+//     webkitRequestAnimationFrame || msRequestAnimationFrame;
+// if (raf) {
+//     raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+// } else {
+//     window.addEventListener('load', loadDeferredStyles);
+// }
 
 // Botões de Controle
 function criarControle() {
