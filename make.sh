072f313ae053b4783149d2a6ef5ce39cf001242f
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-02 22:00:34 -0300

    Script de inicialização da página.

---
+++ make.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# MDS - Script
+#
+# Script de construção dos templates da página.
+#
+# autor: Marcelo dos Santos
+#
+
+echo "Modificando o diretório para "
+cd stylesheets
+echo $(pwd)
+./make.sh
