3271f7a10674c07c9bdc8a1881d3c8c1e4877c84
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-05 13:32:21 -0300

    Formatação do arquivo JS.

--- javascripts/js.js
+++ javascripts/js.js
@@ -7,10 +7,17 @@
  */
 
 // Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments);
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-55078607-1', 'auto');
 ga('send', 'pageview');
@@ -50,9 +57,17 @@ function criarControle() {
     document.body.appendChild(elementDiv);
 }
 
-function gotoTop() { window.scrollTo(0, 0); }
-function gotoBack() { window.history.back(); }
-function detectMobile() { return window.innerWidth <= 600 || window.innerHeight <= 400; }
+function gotoTop() {
+    window.scrollTo(0, 0);
+}
+
+function gotoBack() {
+    window.history.back();
+}
+
+function detectMobile() {
+    return window.innerWidth <= 600 || window.innerHeight <= 400;
+}
 
 // Event Trigger
 window.onload = function() {
