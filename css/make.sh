a844236b95a21c1dfaf32dda2d3d333b17369da8
Marcelo dos Santos <mdssjc@gmail.com>
2018-04-29 10:28:00 -0300

    Atualizado o build.

--- css/make.sh
+++ css/make.sh
@@ -1,27 +1,25 @@
 #! /bin/bash
 # MDS - CSS Script
 #
-# Script de construção do arquivo (CSS - Cascading Style Sheet).
+# Script de construção do arquivo (CSS - Cascading Style Sheet)
 #
 # autor: Marcelo dos Santos
 #
 
-# Ruby:
-#   gem install sass
-# NodeJS:
-#   npm install -g cssnano cssnano-cli
+# Dependências:
+# - Dart Sass
+# - CSS Nano
+#
 # chmod +x make.sh
 
 echo "Gerando CSS..."
-sass template.scss css-base.css
+sass template.sass css-base.css
 
 #echo "Compactando CSS..."
 #cssnano < css-base.css > custom.css
 cat css-base.css > custom.css
 
 echo "Removendo arquivos temporários..."
-rm -r .sass-cache
 rm -v css-base.css
-rm -v css-base.css.map
 
 echo "Concluído!"
