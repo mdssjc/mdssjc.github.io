f52be69578706253651fc17a82174bdbf40679ea
Marcelo dos Santos <mdssjc@gmail.com>
2023-01-09 22:21:24 -0300

    bump

---
+++ _app/immutable/chunks/2-d6ee359b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b4d3b2d9.js";export{t as component};
---
+++ _app/immutable/chunks/3-5d6166ff.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/about/_page.svelte-ca852f0f.js";export{t as component};
